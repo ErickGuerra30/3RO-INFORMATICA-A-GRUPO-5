80f5b1fa1a7196b814f314bd83d594fda675116e
MCanchingre1 <mathias.canchingre@cbecheverria.edu.ec>
2024-07-21 02:31:34 -0500

    a

--- scripts/about.js
+++ scripts/about.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('contactForm').addEventListener('submit', function(event) {
-      // Evita que el formulario realice el envío real
+
       event.preventDefault();
       
-      // Recarga la página
+
       location.reload();
     });
   });
